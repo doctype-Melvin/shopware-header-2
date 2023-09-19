3c145e882bed6f4ce85aa3caa1f67039d78e275e
Melvin Speckamp <doctype.melvin@gmail.com>
2023-09-19 10:11:35 +0200

    initial commit

---
+++ src/ShopwareHeaderTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace ShopwareHeaderTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class ShopwareHeaderTheme extends Plugin implements ThemeInterface
+{
+}
